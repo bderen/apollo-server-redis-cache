01f01913306dc019819888b8821ce7802fd289ef
Bogdan Deren <b.deren@tmg.nl>
2017-11-24 15:43:01 +0100

    v0.1.4

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ import _Cache from 'ioredis'
 import _hashSum from 'hash-sum'
 import * as _package from './package.json'
 
+class CacheKey {
+  constructor(obj) {
+    this.obj = obj ? obj : {}
+  }
+  get cacheKey() {
+    const key = this.obj.key + ':' + 
+           _package.structureVersion + ':' + 
+           (this.obj.queryOperationName ? this.obj.queryOperationName + ':' : '') + 
+           (this.obj.requestGetQuery ? this.obj.requestGetQuery + ':' : '') + 
+           this.obj.queryHash
+    return key
+  }
+}
+
 export default class {
   constructor(options = { cache: true, key: 'asrc', ttl: 900, stale: 60 }) {
     this.options = options
@@ -18,40 +32,42 @@ export default class {
         return next()
       }
 
-      const queryOperationName = req.body && req.body.operationName ? ':' + req.body.operationName : ''
-      const queryHash = req.body && req.body ? ':' + _hashSum(req.body) : ''
-      const structureVersion = ':' + _package.structureVersion
-      const cacheKey = this.options.key + structureVersion + queryOperationName + queryHash
+      const options = arguments;
+      
+      let binary = false;
+      if ( typeof options[0] === 'object' && typeof options[0].binary === 'boolean' ) {
+        binary = options[0].binary;
+      }
+
+      const requestGetQuery = req.query.query && req.query.query ? _hashSum(req.query.query) : null
+      let queryOperationName;
+      if (Array.isArray(req.body)) {
+        const names = req.body.filter(q => q.operationName).map(q => q.operationName)
+        queryOperationName = names && names.length ? names.join(',') : null
+      } else {
+        queryOperationName = req.body.operationName ? req.body.operationName : null
+      }
+      const queryHash = req.body && req.body ? _hashSum(req.body) : ''
+
+      const cacheKey = new CacheKey({
+        key: this.options.key,
+        requestGetQuery,
+        queryOperationName,
+        queryHash,
+      }).cacheKey
+
+      queryOperationName = null;
 
       const _write = res.write.bind(res);
 
       this.client.hgetall(cacheKey, (err, result) => {
         if ( result && result.body && result.body.length ) {
-          const now = +new Date()
-          const created = result.created
-          const stale = result.stale * 1000
-          const expired = ( now - created ) > stale
-          
-          if ( expired ) {
-            let entry = {
-              body: result.body,
-              stale: result.stale,
-              created: +new Date(),
-            }
-            this.client.hmset(cacheKey, entry)
-            if (this.options.httpHeader) {
-              res.setHeader(`${this.options.httpHeader}`, 'MISS')
-            }
-            entry = null;
-            return next()
-          } else {
-            if (this.options.httpHeader) {
-              res.setHeader(`${this.options.httpHeader}`, 'HIT')
-            }
-            res.setHeader('Content-Type', 'application/json');
-            _write(result.body);
-            res.end();
+          if (this.options.httpHeader) {
+            res.setHeader(`${this.options.httpHeader}`, 'HIT')
           }
+          res.setHeader('Content-Type', 'application/json');
+          _write(result.body);
+          res.end();
         } else {
           if (this.options.httpHeader) {
             res.setHeader(`${this.options.httpHeader}`, 'MISS')
@@ -61,6 +77,10 @@ export default class {
       });
 
       res.write = (body) => {
+        if(binary && typeof body !== 'string'){
+          body = new Buffer(body).toString('base64');
+        }
+
         if ( typeof body !== 'string' ) {
           _write(body);
           res.end();
@@ -75,6 +95,7 @@ export default class {
         this.client.hmset(cacheKey, entry);
         this.client.expire(cacheKey, this.options.ttl);
         entry = null;
+        
         _write(body);
         res.end();
       }
